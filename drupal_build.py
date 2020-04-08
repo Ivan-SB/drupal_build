b3ac6abaa925bd7440eb653167066a5c215c4bc5
Ivan Sergio Borgonovo <mail@webthatworks.it>
2020-04-08 19:13:41 +0200

    consolidated code to get rev in one function prepare to support different actions (install, download, composer...)

--- drupal_build.py
+++ drupal_build.py
@@ -15,6 +15,7 @@ with open('test.txt', 'w') as f:
     process = subprocess.Popen(['ls', '-l'], stdout=f)
 """
 
+import os
 import sys
 import signal
 
@@ -55,6 +56,27 @@ def zeroOnNone(x):
   return int(x) if x is not None else 0
 
 
+def getVersion(repo, refilter):
+  for ref in g.ls_remote(repo).split('\n'):
+    dmatch = re.search(refilter, ref)
+    if(dmatch is not None):
+      va = [ zeroOnNone(dmatch.group(6)),
+          zeroOnNone(releasea[dmatch.group(5)]),
+          zeroOnNone(dmatch.group(4)),
+          zeroOnNone(dmatch.group(3)),
+          zeroOnNone(dmatch.group(2))]
+      v = va[0] + 100 * va[1] + 100 ** 2 * va[2] + 100 ** 3 * va[3] + 100 ** 4 * va[4]
+      a[dmatch.group(1)] = (v, va)
+  vl = sorted(a.items(), key=lambda kv:(kv[1][0]))
+  return vl
+
+def createWorkingDir(workdir):
+  if workdir is None:
+    pass
+  else:
+    return "use temp" 
+  
+
 class Drupal():
   pass
 
@@ -69,9 +91,9 @@ if __name__ == "__main__":
                         metavar='BASEVERSION',
                         help='main base version: 7, 8, 9...',
                         type=int)
-  parser.add_argument('-d', '--dev',
-                        dest='dev',
-                        metavar='DEV',
+  parser.add_argument('-r', '--release',
+                        dest='release',
+                        metavar='RELEASE',
                         help='minimum required dev version (dev, alpha, beta, rc), stable if omitted')
   parser.add_argument('-m', '--modules',
                         dest='modules',
@@ -87,6 +109,13 @@ if __name__ == "__main__":
                         metavar='PATH',
                         required=True,
                         help='path')
+  parser.add_argument('-a', '--action',
+                        dest='action',
+                        choices=('download', 'composer', 'install'),
+#                         metavar='ACTION',
+                        type=str.lower,
+#                         required=True,
+                        help='action [download (just download), install (install modules and themes from tar), composer (install modules and themes with composer)] ')
 
   args = parser.parse_args()
 
@@ -94,10 +123,11 @@ if __name__ == "__main__":
   OB = OnBreak(drupal)
 
   base = args.base
-  dev = args.dev
+  release = args.release
   modules = args.modules
+  action = args.action
 
-  deva = { 'dev': 0, 'alpha': 1, 'beta': 2, 'rc': 3, None: 4 }
+  releasea = { 'dev': 0, 'alpha': 1, 'beta': 2, 'rc': 3, None: 4 }
   g = git.cmd.Git()
   # 9.0.0-beta2
   # 5 4 3    21
@@ -107,62 +137,36 @@ if __name__ == "__main__":
   dmrerev = re.compile(r"[^ \t]+[ \t]+refs/tags/(([0-9]+)\.x-([0-9]+)?\.?([0-9]+)?-?(dev|alpha|beta|rc)?([0-9]+)?).*")
   a = {}
   va = []
-  for ref in g.ls_remote(DREPOSITORY).split('\n'):
-    dmatch = re.search(drerev, ref)
-    if(dmatch is not None):
-      va = [ zeroOnNone(dmatch.group(6)),
-            zeroOnNone(deva[dmatch.group(5)]),
-            zeroOnNone(dmatch.group(4)),
-            zeroOnNone(dmatch.group(3)),
-            zeroOnNone(dmatch.group(2))]
-      v = va[0] + 100 * va[1] + 100 ** 2 * va[2] + 100 ** 3 * va[3] + 100 ** 4 * va[4]
-      a[dmatch.group(1)] = (v, va)
-  vl = sorted(a.items(), key=lambda kv:(kv[1][0]))
-  rev_temp = vl
+
+  rev_temp = getVersion(DREPOSITORY, drerev)
   if(base is not None):
     rev_temp = [v for v in rev_temp if (v[1][0] >= base * 100 ** 4 and v[1][0] < (base + 1) * 100 ** 4)]
-  rev_temp = [v for v in rev_temp if (v[1][1][1] >= deva[dev])]
+  rev_temp = [v for v in rev_temp if (v[1][1][1] >= releasea[release])]
 
   file = DFILE.format(rev_temp[-1][0])
+  
   url = DTAR.format(file)
   print(url)
+  
+  if modules is not None:
+    for m in modules:
+      a = {}
+      va = []
+      dmrepo = DMREPOSITORY.format(m)
+      rev_temp = getVersion(dmrepo, dmrerev)
+      if(base is not None):
+        rev_temp = [v for v in rev_temp if (v[1][0] >= base * 100 ** 4 and v[1][0] < (base + 1) * 100 ** 4)]
+      rev_temp = [v for v in rev_temp if (v[1][1][1] >= releasea[release])]
+  
+      file = DMFILE.format(m, rev_temp[-1][0])
+      url = DTAR.format(file)
+      print(url)
 
 #   http = urllib3.PoolManager()
 #   r = http.request('GET', url)
 #   f = open(file, 'wb')
 #   f.write(r.data)
-#   f.close  
-
-  for m in modules:
-    a = {}
-    va = []
-    dmrepo = DMREPOSITORY.format(m)
-    for ref in g.ls_remote(dmrepo).split('\n'):
-      dmatch = re.search(dmrerev, ref)
-      if(dmatch is not None):
-        va = [ zeroOnNone(dmatch.group(6)),
-            zeroOnNone(deva[dmatch.group(5)]),
-            zeroOnNone(dmatch.group(4)),
-            zeroOnNone(dmatch.group(3)),
-            zeroOnNone(dmatch.group(2))]
-        v = va[0] + 100 * va[1] + 100 ** 2 * va[2] + 100 ** 3 * va[3] + 100 ** 4 * va[4]
-        a[dmatch.group(1)] = (v, va)
-    vl = sorted(a.items(), key=lambda kv:(kv[1][0]))
-    rev_temp = vl
-    if(base is not None):
-      rev_temp = [v for v in rev_temp if (v[1][0] >= base * 100 ** 4 and v[1][0] < (base + 1) * 100 ** 4)]
-    rev_temp = [v for v in rev_temp if (v[1][1][1] >= deva[dev])]
-
-    file = DMFILE.format(m, rev_temp[-1][0])
-    url = DTAR.format(file)
-    print(url)
-  
-  
-
-
+#   f.close
 
   print("OK")
-
-#   pprint.pprint()
-#   z[-1] last
 
